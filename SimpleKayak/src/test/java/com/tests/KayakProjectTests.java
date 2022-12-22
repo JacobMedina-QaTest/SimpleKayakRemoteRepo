7e310c365ad0d6cb5f2c1f07dad295a09c1c295e
Jacob.Medina <jrmQaTest@gmail.com>
2022-12-22 10:12:32 -0500

    Full Flight Checkout w/ Result Printout

--- SimpleKayak/src/test/java/com/tests/KayakProjectTests.java
+++ SimpleKayak/src/test/java/com/tests/KayakProjectTests.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.annotations.Test;
@@ -37,60 +38,86 @@ public class KayakProjectTests extends Base{
 			myGSLibrary.customWait(1);
 			log.info("Click on 'Round Trip' dropdown element");
 
-			By travelerLocator = RelativeLocator.with(By.tagName("span")).toRightOf(By.xpath("//div[@class='wIIH wIIH-mod-size-small wIIH-mod-state-default wIIH-pres-default wIIH-mod-variant-default wIIH-mod-radius-none wIIH-mod-full-width wIIH-mod-dirty']"));
-			
 	//Two Adults
-//			myGSLibrary.clickElement(travelerLocator);
-//			myGSLibrary.customWait(1);
-//			myGSLibrary.clickElement(mylocator.getLocator("AddTravelorBtn"));
-//			myGSLibrary.customWait(1);
-//			log.info("Selected 2 Travelers");
+			myGSLibrary.clickElement(mylocator.getLocator("AdultTravelerDropDown"));
+			myGSLibrary.customWait(1);
+			List<WebElement> addOneAdult = driver.findElements(By.xpath("//button[@tabindex='-1'][@aria-label='Increment']"));
+			addOneAdult.get(0).click();
+			myGSLibrary.customWait(3);
+			log.info("Selected 2 Travelers");
 			
-	//First Flight
+	//First Class Flight
+//			myGSLibrary.clickElement(mylocator.getLocator("FlightDropDown"));
+//			myGSLibrary.customWait(1);
+//			myGSLibrary.clickElement(mylocator.getLocator("FirstClassBtn"));
+//			myGSLibrary.customWait(1);
+//			log.info("Selected First Class Flight");
+			
+	//Economy Class FLight
 			myGSLibrary.clickElement(mylocator.getLocator("FlightDropDown"));
 			myGSLibrary.customWait(1);
-			myGSLibrary.clickElement(mylocator.getLocator("FirstClassBtn"));
+			myGSLibrary.clickElement(mylocator.getLocator("EconomyClassBtn"));
 			myGSLibrary.customWait(1);
-			log.info("Selected First Class Flight");
+			log.info("Selected Economy Class Flight");
 			
 	//Checked bags
 			myGSLibrary.clickElement(mylocator.getLocator("BagDrop"));
 			myGSLibrary.customWait(1);
 			myGSLibrary.clickElement(mylocator.getLocator("BagDropDown"));
 			myGSLibrary.customWait(1);
+			List<WebElement> addCheckBag = driver.findElements(By.xpath("//button[@tabindex='-1'][@aria-label='Increment']"));
+			addCheckBag.get(1).click();
+			myGSLibrary.customWait(1);
+			addCheckBag.get(1).click();
+			myGSLibrary.customWait(1);
+			//myGSLibrary.clickElement(By.xpath(webUrl));
 			log.info("Selected Check Bags");
 			
-	//From Washington, DC	
-			myGSLibrary.clickElement(mylocator.getLocator("SelectDCDeparture"));
-			myGSLibrary.customWait(1);
-			log.info("Selected Departure Location");
+	//From Washington, DC	---> This section has been auto generated
+//			myGSLibrary.clickElement(mylocator.getLocator("SelectDCDeparture"));
+//			myGSLibrary.customWait(1);
+//			log.info("Selected Departure Location");
 			
 	// To Honolulu, Hawaii
-			myGSLibrary.clickElement(By.cssSelector("input[placeholder='To?']"));
+//			myGSLibrary.clickElement(By.cssSelector("input[placeholder='To?']"));
+//			myGSLibrary.customWait(2);
+//			WebElement Destination = myGSLibrary.waitForElementVisibility(By.xpath("//input[@placeholder='To?']"));
+//			Destination.sendKeys("Honolulu, Hawaii");
+//	        myGSLibrary.customWait(1);
+	        
+	//To Denver, Colorado
+	        myGSLibrary.clickElement(By.cssSelector("input[placeholder='To?']"));
 			myGSLibrary.customWait(2);
-			WebElement Destination = myGSLibrary.waitForElementVisibility(By.xpath("//input[@placeholder='To?']"));
-			Destination.sendKeys("Honolulu, Hawaii");
-	        myGSLibrary.customWait(1);
+			WebElement Destination2 = myGSLibrary.waitForElementVisibility(By.xpath("//input[@placeholder='To?']"));
+			Destination2.sendKeys("Denver, Colorado");
+			myGSLibrary.customWait(2);
+			Destination2.sendKeys(Keys.ENTER);
+			log.info("To Destination Inserted");
 	        
 	  //Departure date
 	        myGSLibrary.clickElement(By.xpath("//span[@aria-label='Start date calendar input']"));
 	        myGSLibrary.customWait(1);
-	        myGSLibrary.clickElement(By.xpath("//button[@aria-label='Next Month']//span[@class='svg tUEz-icon']//*[name()='svg']"));
+	        myGSLibrary.clickElement(mylocator.getLocator("NavigateMnthRightArrow"));
 	        myGSLibrary.customWait(0.5);
-	        myGSLibrary.clickElement(By.xpath("//button[@aria-label='Next Month']//span[@class='svg tUEz-icon']//*[name()='svg']"));
+	        myGSLibrary.clickElement(mylocator.getLocator("NavigateMnthRightArrow"));
 	        myGSLibrary.customWait(0.5);
-	        myGSLibrary.clickElement(By.xpath("//button[@aria-label='Next Month']//span[@class='svg tUEz-icon']//*[name()='svg']"));
+	        myGSLibrary.clickElement(mylocator.getLocator("NavigateMnthRightArrow"));
 	        myGSLibrary.customWait(0.5);
-	        //myGSLibrary.clickElement(By.xpath("//button[@aria-label='Next Month']//span[@class='svg tUEz-icon']//*[name()='svg']"));
+	        myGSLibrary.clickElement(mylocator.getLocator("NavigateMnthRightArrow"));
 	        myGSLibrary.customWait(0.5);
-	        myGSLibrary.clickElement(By.xpath("//div[@aria-label='February 21, 2023']"));
-	        myGSLibrary.customWait(1);      
-	   //return date
-	        myGSLibrary.clickElement(By.xpath("//div[@aria-label='March 7, 2023']")); 
-	        myGSLibrary.customWait(1);
-	        myGSLibrary.clickElement(By.xpath("//button[@aria-label='Search']"));
-	        myGSLibrary.customWait(30);
+	        myGSLibrary.clickElement(By.xpath("//div[@aria-label='Thursday May 25, 2023']")); //Selecting departure date of 5/25/23
+	        myGSLibrary.customWait(3);  
+	        log.info("Departure date is selected");
 	        
+	   //Return date
+	        myGSLibrary.clickElement(By.cssSelector("div[class='mkUa mkUa-pres-mcfly mkUa-mod-legend-orange'][aria-label='Friday June 2, 2023']")); //Selecting return date of 6/2/23
+	        myGSLibrary.customWait(3);
+	        log.info("Return date is selected");
+	        
+	   //Search Button     
+	        myGSLibrary.clickElement(By.xpath("//button[@aria-label='Search']"));
+	        myGSLibrary.customWait(7);
+	        log.info("Search button clicked...");
 	        
 	   //print information of tickets
 	       
@@ -99,14 +126,14 @@ public class KayakProjectTests extends Base{
 //	        	 String Data = Results.get(i).getText(); 
 //	        	 System.out.println("Results: " +Data); }  
 		
-	        WebElement searchResultRegion = driver.findElement(By.cssSelector("#searchResultsList"));
+	        WebElement searchResultRegion = driver.findElement(By.cssSelector("div.Ui-Flights-Results-Components-ListView-container "));
 	        myGSLibrary.highlightWebElement(searchResultRegion);
 	        
 	       // myGSLibrary.highlightWebElement(By.cssSelector("div.inner-grid.keel-grid"));
 	        
-	        List<WebElement> resultTickets = searchResultRegion.findElements(By.cssSelector("div.inner-grid.keel-grid"));
+	        List<WebElement> resultTickets = searchResultRegion.findElements(By.cssSelector("div.nrc6"));
 	        int size = resultTickets.size();
-	        System.out.println("Total TIckets: " + size);
+	        System.out.println("Total Tickets: " + size);
 	        
 	        int counter = 1;
 	        for(WebElement ticket : resultTickets) {
@@ -114,24 +141,17 @@ public class KayakProjectTests extends Base{
 	        	if(counter > 5) {
 	        		break;
 	        	}
-	        	
 	        	//myGSLibrary.scrollIntoView(ticket);
-	        	
 	        	myGSLibrary.customWait(0.25);
-	        	
-	        	
 	        	myGSLibrary.highlightWebElement(ticket);
 	        	String ticketTxt = ticket.getText();
-	        	System.out.println("TIcket > " + counter);
+	        	System.out.println(" ");
+	        	System.out.println("<----->");
+	        	System.out.println(" ");
+	        	System.out.println("Ticket > " + counter);
 	        	System.out.println(ticketTxt);
 	        	counter ++;
 	        }
-	        
-	        // List<WebElement> resultWrapperItems = searchResultRegion.findElements(By.className("resultWrapper"));
-	        
-	        
-	       	
-			
 		} catch (Exception e) {
 			log.error("Error: ", e);
 			assertEquals(true, false);
